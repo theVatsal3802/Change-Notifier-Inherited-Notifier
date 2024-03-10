e7cf77d84926b162d558ee0eee53027d2125abfb
Vatsal Adhiya <vatsaladhiya248@gmail.com>
2024-03-10 18:31:37 +0530

    Step 2 - Implemeneted Change Notifier and Inherited Notifier

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SliderData extends ChangeNotifier {
+  double _value = 0;
+
+  double get value => _value;
+
+  set value(double newValue) {
+    if (newValue != value) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
+
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier({
+    super.key,
+    required super.notifier,
+    required super.child,
+  });
+
+  static double of(BuildContext context) {
+    return context
+            .dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
+            ?.notifier
+            ?.value ??
+        0;
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -27,6 +58,43 @@ class HomeScreen extends StatelessWidget {
         title: const Text(
           'Home',
           textScaler: TextScaler.noScaling,
+        ),
+      ),
+      body: SliderInheritedNotifier(
+        notifier: sliderData,
+        child: Builder(
+          builder: (builderContext) {
+            return Column(
+              children: [
+                Slider.adaptive(
+                  value: SliderInheritedNotifier.of(builderContext),
+                  onChanged: (value) {
+                    sliderData.value = value;
+                  },
+                ),
+                Row(
+                  children: [
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(builderContext),
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.5,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(builderContext),
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.5,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
